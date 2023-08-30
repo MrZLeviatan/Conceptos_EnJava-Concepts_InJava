f0e58f44f93980ef30cc189cb80fc9faf7d30c47
Nicolas <117557162+MrZLeviatan@users.noreply.github.com>
2023-08-30 01:43:48 -0500

    Se agrega el primer ejemplo
    Se agrega el primer ejemplo de recursividad al repositorio UwU

---
+++ Recursividad/Ejercicio1/src/main/java/MrZLeviatan/Main.java
@@ -0,0 +1,61 @@
+package MrZLeviatan;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    /*
+        CASO BASE:
+
+            -CUANDO EL NUMERO SEA MENOR QUE 10, LA RESPUESTA ES 1 DIGITO/CIFRA
+          
+        CASO RECURSIVO:
+
+            -DIVIDIR EL NUMERO ENTRE 10
+            -CONTAR +1 DIGITO OH EN EL SUMATORIO
+            -LLAMAR LA RECURSIVIDAD, CON EL NUEVO VALOR
+  */
+    public static int contarDigitos ( int numero){
+        int res;
+        if (numero < 10) {
+            res = 1;
+
+        } else {
+            numero = numero / 10;
+            res = 1 + contarDigitos(numero);
+        }
+        return res;
+    }
+
+
+
+    public static void obtenerDatos () {
+
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        System.out.println(" Digite un numero Por favor: ");
+
+
+            try {
+                numero = sc.nextInt();
+                int sumaDigitos = contarDigitos(numero);
+                System.out.println("La cantidad de digitos en el anterior ejemplo es: " + sumaDigitos);
+
+            } catch (InputMismatchException e) {
+                System.out.println("Error: " + "El valor tecleado debe ser un entero");
+                obtenerDatos();
+
+            }
+    }
+    public static void main(String[] args) {
+
+        System.out.println(" --EJERCICIO 1-- ");
+        obtenerDatos();
+
+    }
+
+}
+
+
+
